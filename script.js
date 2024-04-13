89b7e9bcdad1b5e574b23b526b7cacef779a7056
Dotda11 <164742583+Dotda11@users.noreply.github.com>
2024-04-13 15:42:59 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const actionbtn = document.querySelector('.actionbtn');
-const navbar = document.querySelector('.navbar')
+const navbar = document.querySelector('.navbar');
+const banner = document.querySelector('.banner');
 
 actionbtn.addEventListener('click',()=>{
   actionbtn.classList.toggle('active');
   navbar.classList.toggle('active');
+  banner.classList.toggle('active');
 })
