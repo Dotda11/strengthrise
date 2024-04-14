7af13ff2a6a98ade7f55bca287cd9ad2244d9523
Dotda11 <164742583+Dotda11@users.noreply.github.com>
2024-04-14 20:51:44 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 var menuBtn = document.getElementById("menuBtn")
+var sideNav = document.getElementById("sideNav")
+var menu = document.getElementById("menu")
+
+menuBtn.onclick = function(){
+  if(sideNav.style.right == "-250px"){
+    sideNav.style.right = "0";
+  }
+}
