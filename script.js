830219cbdb820e842d0963c2fdf39aaf8105d4d0
Dotda11 <164742583+Dotda11@users.noreply.github.com>
2024-04-14 20:44:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const actionbtn = document.querySelector('.actionbtn');
-const navbar = document.querySelector('.navbar');
-const banner = document.querySelector('.banner');
-
-actionbtn.addEventListener('click',()=>{
-  actionbtn.classList.toggle('active');
-  navbar.classList.toggle('active');
-  banner.classList.toggle('active');
-})
+var menuBtn = document.getElementById("menuBtn")
